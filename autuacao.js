bc697b8c9354ddf4c7e7d32601e944b16adf301a
exebixel <ezequielnat7@gmail.com>
2020-11-01 10:21:21 -0300

    Finish ifs in function autuacao

--- autuacao.js
+++ autuacao.js
@@ -11,10 +11,26 @@ function get_data() {
     return {"max": max, "speed": speed}
 }
 
+function percent(total, value) {
+    return (value*100/100)-100
+}
+
 function autuacao(event) {
     const {max, speed} = get_data()
-    if (speed - max <= 7) {
-        console.log("here")
-        
+
+    if ( speed - max <= 0 ) {
+        console.log("não foi autuado")
+    }
+    else if (speed - max <= 7) {
+        console.log("até 7")
+    } 
+    else if (percent(max, speed) <= 20) {
+        console.log("maior que 7 menor que 20%")
+    } 
+    else if ( percent(max,speed) <= 50) {
+        console.log("maior que 20% menor que 50%")
+    }
+    else if ( percent(max,speed) > 50) {
+        console.log("maior que 50%")
     }
 }
